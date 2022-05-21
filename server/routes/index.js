b0f7e41e4b75bbfc14d4bfbfd45ce8e52b74da94
srihari <hsri0007@gmail.com>
2022-05-21 22:26:35 +0530

    initial

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,20 +3,20 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/products", IndexController.getAllproducts);
-router.post("/products/add", IndexController.postProducts);
-router.post("/products/update", IndexController.updateProducts);
-router.get("/products/admin", IndexController.getAllproductsbyAdmin);
-router.post("/auth/createuser", IndexController.createUser);
-router.post("/auth/loginuser", IndexController.loginUser);
-router.post("/auth/updateuser", IndexController.updateUser);
-router.get("/dashboard_folders", IndexController.getAllDashBoardFolders);
-router.get("/dashboard_folders/admin", IndexController.getAllDashBoardsFoldersforAdmin);
-router.post("/dashboard_folders/add", IndexController.postDashBoardFolders);
-router.post("/dashboard_folders/update", IndexController.updateDashBoardFolders);
-router.get("/dashboards", IndexController.getAllDashBoards);
-router.get("/dashboards/admin", IndexController.getAllDashBoardsforAdmin);
-router.post("/dashboards/add", IndexController.postDashBoard);
-router.post("/dashboards/update", IndexController.updateDashBoard);
+// router.get("/products", IndexController.getAllproducts);
+// router.post("/products/add", IndexController.postProducts);
+// router.post("/products/update", IndexController.updateProducts);
+// router.get("/products/admin", IndexController.getAllproductsbyAdmin);
+// router.post("/auth/createuser", IndexController.createUser);
+// router.post("/auth/loginuser", IndexController.loginUser);
+// router.post("/auth/updateuser", IndexController.updateUser);
+// router.get("/dashboard_folders", IndexController.getAllDashBoardFolders);
+// router.get("/dashboard_folders/admin", IndexController.getAllDashBoardsFoldersforAdmin);
+// router.post("/dashboard_folders/add", IndexController.postDashBoardFolders);
+// router.post("/dashboard_folders/update", IndexController.updateDashBoardFolders);
+// router.get("/dashboards", IndexController.getAllDashBoards);
+// router.get("/dashboards/admin", IndexController.getAllDashBoardsforAdmin);
+// router.post("/dashboards/add", IndexController.postDashBoard);
+// router.post("/dashboards/update", IndexController.updateDashBoard);
 
 module.exports= router;
