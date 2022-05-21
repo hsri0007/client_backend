18cbb626ecb9e0a43bf9e83a1eedf2d8d8622561
srihari <hsri0007@gmail.com>
2022-05-21 22:25:22 +0530

    initial

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const indexRouter = require("./routes/index");
+// const indexRouter = require("./routes/index");
 const express = require("express");
 const logger = require("morgan");
 // import usersRouter from "./routes/users";
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(logger("dev"));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use("/api/v1", indexRouter);
+// app.use("/api/v1", indexRouter);
 // app.use("/api/v1/users", usersRouter);
 // app.use("/api/v1/admin", adminRouter);
 
