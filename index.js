5443a20b40333594c52d675ef849663cf7b5f006
srihari <hsri0007@gmail.com>
2022-05-22 17:09:40 +0530

    chnages

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
 app.get('/contact', (req, res) => res.send('Contact Page Route'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
